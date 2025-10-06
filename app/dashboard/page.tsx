3093e67aa0ca8570224b611f97f2aa5db62d449c
Куимов Андрей <a.kuimov.inbox@gmail.com>
2025-10-06 21:17:46 +0700

    start to design right panel with price

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -7,6 +7,12 @@ import {Separator} from "@/components/ui/separator";
 import {useAuth} from "@/context/AuthContext";
 import TerminalHardwareTable from "@/components/ui/my-components/terminal-hardware-table";
 import {Card, CardAction, CardContent, CardDescription, CardFooter, CardHeader, CardTitle} from "@/components/ui/card";
+import {RussianRuble} from "lucide-react";
+import {InputGroup, InputGroupAddon, InputGroupButton, InputGroupInput} from "@/components/ui/input-group";
+import {Switch} from "@/components/ui/switch";
+import {Label} from "@/components/ui/label";
+import {Field, FieldLabel} from "@/components/ui/field";
+import {Select, SelectContent, SelectItem, SelectTrigger, SelectValue} from "@/components/ui/select";
 const OPTIONS = [
     { value: 'nextjs', label: 'Next.js' },
     { value: 'react', label: 'React' },
@@ -14,6 +20,8 @@ const OPTIONS = [
     { value: 'zustand', label: 'Zustand' },
     { value: 'prisma', label: 'Prisma' },
 ];
+
+
 const Page = () =>{
     const { user } = useAuth();
     const [selected, setSelected] = useState<Option[]>([]);
@@ -40,19 +48,60 @@ const Page = () =>{
             {/*</Breadcrumb>*/}
         </div>
     </header>
-        <div className="flex flex-1 flex-row px-4">
-            <div className="@container/main flex flex-1 flex-row gap-2">
+        <div className="flex flex-1 flex-col px-4">
+            <div className="@container/main gap-2">
+                <h1 className="scroll-m-20 text-2xl font-bold tracking-tight text-balance">UTS Fly W 55/65/75</h1>
+            </div>
+            <div className="@container/main flex flex-1 flex-row gap-3">
                 <div className="flex flex-col flex-1 gap-4 py-4 md:gap-6 md:py-6">
                     <TerminalHardwareTable />
                 </div>
                 <Card className="w-full max-w-[450px]">
                     <CardHeader>
-                        <CardTitle>Card Title</CardTitle>
-                        <CardDescription>Card Description</CardDescription>
-                        <CardAction>Card Action</CardAction>
+                        <CardTitle className="flex gap-2 items-end text-4xl font-semibold tabular-nums @[250px]/card:text-3xl">144 375 <span>&#8381;</span></CardTitle>
+                        <CardAction>
+                            <h2 className="text-xl">Итоговая цена</h2>
+                            <CardDescription>коэффициент - 1</CardDescription>
+                        </CardAction>
                     </CardHeader>
-                    <CardContent>
-                        <p>Card Content</p>
+                    <CardContent className="flex flex-col gap-3">
+                        <InputGroup>
+                            <InputGroupAddon>
+                                &#8381;
+                            </InputGroupAddon>
+                            <InputGroupInput placeholder="255 564"  />
+                            <InputGroupAddon align="inline-end">
+                                <div className="flex items-center space-x-2">
+                                    <Label htmlFor="airplane-mode">Фиксированная цена</Label>
+                                    <Switch id="airplane-mode" />
+                                </div>
+                            </InputGroupAddon>
+                        </InputGroup>
+
+                        <Field orientation="horizontal">
+                            <FieldLabel>Плательщик НДС:</FieldLabel>
+                            <Select>
+                                <SelectTrigger className="w-full md:w-1/2">
+                                    <SelectValue  />
+                                </SelectTrigger>
+                                <SelectContent>
+                                    <SelectItem value="engineering">ООО "УТС"</SelectItem>
+                                    <SelectItem value="design">ИП Куимов</SelectItem>
+                                </SelectContent>
+                            </Select>
+                        </Field>
+                        <div className="flex items-center">
+                            <p className="flex-1">В том числе НДС 5%</p>
+                            <p className="text-right text-xl">6 875 &#8381;</p>
+                        </div>
+                        <div className="flex items-center">
+                            <p className="flex-1">Цена для клиента</p>
+                            <p className="text-right text-xl">137 500 &#8381;</p>
+                        </div>
+                        <div className="flex items-center">
+                            <p className="flex-1">Цена без округления</p>
+                            <p className="text-right text-xl">137 500 &#8381;</p>
+                        </div>
                     </CardContent>
                     <CardFooter>
                         <p>Card Footer</p>
--- components/ui/my-components/terminal-hardware-table.tsx
+++ components/ui/my-components/terminal-hardware-table.tsx
@@ -1,43 +1,54 @@
-import {Trash2} from "lucide-react";
+import {GripVertical, Trash2} from "lucide-react";
 import {Table, TableBody, TableCaption, TableCell, TableHead, TableHeader, TableRow} from "../table";
 import {Input} from "@/components/ui/input";
 import {Checkbox} from "@/components/ui/checkbox";
 import {Button} from "@/components/ui/button";
+import {RadioGroup, RadioGroupItem} from "@/components/ui/radio-group";
 
 const TerminalHardwareTable = () => {
-    return (<Table>
-        <TableCaption>A list of your recent invoices.</TableCaption>
-        <TableHeader>
-            <TableRow>
-                <TableHead className="w-[20px]"></TableHead>
-                <TableHead>Название</TableHead>
-                <TableHead>Внутреннее название</TableHead>
-                <TableHead>Основное</TableHead>
-                <TableHead>Опция</TableHead>
-                <TableHead>Цена</TableHead>
-                <TableHead>Итого (руб)</TableHead>
-                <TableHead>Прибыль</TableHead>
-                <TableHead></TableHead>
-            </TableRow>
-        </TableHeader>
-        <TableBody>
-            <TableRow>
-                <TableCell></TableCell>
-                <TableCell>LCD LED 55" (139,7 см.) 3840x2160 (4K)</TableCell>
-                <TableCell>AMD Ryzen 5 с видеоядром АМ4</TableCell>
-                <TableCell><Checkbox /></TableCell>
-                <TableCell><Checkbox /></TableCell>
-                <TableCell>54999</TableCell>
-                <TableCell>65998.80</TableCell>
-                <TableCell>10999.80</TableCell>
-                <TableCell>
-                    <Button variant="secondary" size="icon" className="size-8">
-                        <Trash2 />
-                    </Button>
-                </TableCell>
-            </TableRow>
-        </TableBody>
-    </Table>);
+    return (
+        <div className="overflow-hidden rounded-md border">
+            <Table>
+                <TableCaption>A list of your recent invoices.</TableCaption>
+                <TableHeader>
+                    <TableRow>
+                        <TableHead className="w-[20px]"></TableHead>
+                        <TableHead>Название</TableHead>
+                        <TableHead>Внутреннее название</TableHead>
+                        <TableHead>Основное</TableHead>
+                        <TableHead>Опция</TableHead>
+                        <TableHead>Цена</TableHead>
+                        <TableHead>Итого (руб)</TableHead>
+                        <TableHead>Прибыль</TableHead>
+                        <TableHead></TableHead>
+                    </TableRow>
+                </TableHeader>
+                <TableBody>
+                    <TableRow>
+                        <TableCell><GripVertical color="grey" size={14}/></TableCell>
+                        <TableCell>LCD LED 55" (139,7 см.) 3840x2160 (4K)</TableCell>
+                        <TableCell>AMD Ryzen 5 с видеоядром АМ4</TableCell>
+                        <TableCell><Checkbox /></TableCell>
+                        <TableCell>
+                            <RadioGroup
+                                className="flex flex-col"
+                            >
+                                <RadioGroupItem value="all" />
+                            </RadioGroup>
+                        </TableCell>
+                        <TableCell>54999</TableCell>
+                        <TableCell>65998.80</TableCell>
+                        <TableCell>10999.80</TableCell>
+                        <TableCell>
+                            <Button variant="secondary" size="icon" className="size-8">
+                                <Trash2 />
+                            </Button>
+                        </TableCell>
+                    </TableRow>
+                </TableBody>
+            </Table>
+        </div>
+    );
 }
 
 export default TerminalHardwareTable;
